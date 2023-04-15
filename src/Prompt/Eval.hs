b88f0741ce2f7d05256ce46f374b8bc779ce7e99
vonabarak <github@vonabarak.ru>
2023-04-16 01:10:21 +0200

    Run applications in app.slice

--- src/Prompt/Eval.hs
+++ src/Prompt/Eval.hs
@@ -1,4 +1,4 @@
-module EvalPrompt (evalPrompt) where
+module Prompt.Eval (evalPrompt) where
 
 import XMonad
 import XMonad.Prompt
@@ -6,7 +6,7 @@ import Data.List ( intercalate )
 import qualified Language.Haskell.Interpreter  as I
 
 -- Eval prompt.
--- evaluate any haskell expression by pressing mod-shift-x
+-- evaluate any haskell expression
 data EvalPrompt = EvalPrompt
 
 instance XPrompt EvalPrompt where
